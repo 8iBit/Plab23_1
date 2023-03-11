b35be3fb64480b2e34b9d773ea1a576545ddc2ea
Porrapat <iporrapat@gmail.com>
2023-03-11 19:46:33 +0700

    average.exe

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
 
+int main(int argc,char *argv[])
+{
+    float sum=0;
+    double a = argc-1;
+    if(argc == 1) cout << "Please input numbers to find average.";
+    else 
+    {
+        for(int i=0;i<argc-1;i++)
+        {
+            sum += atof(argv[i+1]);
+        }
+        cout << "---------------------------------\n";
+        cout << "Average of " << a << " numbers = " << sum/a << "\n";
+        cout << "---------------------------------\n";
+    }
+}
